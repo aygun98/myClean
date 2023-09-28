67a063fc2b739e3512b3321fe3d3eac021070c78
Aygun <aygun.xuduyeva98@gmail.com>
2023-09-28 08:25:57 -0700

    first commit

---
+++ svg/index.js
@@ -0,0 +1,12 @@
+const bars = document.querySelector('.nav-bars-btn')
+const back = document.querySelector('.fa-chevron-btn')
+const overLay = document.querySelector('.overlay-menu')
+
+console.log(overLay);
+
+bars.addEventListener('click', ()=>{
+    overLay.classList.add('active')
+})
+back.addEventListener('click', ()=>{
+    overLay.classList.remove('active')
+})
